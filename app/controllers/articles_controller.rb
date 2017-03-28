1295f71bc8a1b621680346fa19cbf1e849610de7
Marcela Porto <email": null>
2017-03-28 10:52:51 -0700

    Adds destroy/update and flash notifications

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -8,11 +10,40 @@ class ArticlesController < ApplicationController
     @article = article_find
   end
 
-
-private
-
-  def article_find
-    Article.find(params[:id])
+  def new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    flash.notice = "Article '#{@article.title}' created!"
+
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = article_find
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' deleted!"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = article_find
+  end
+
+  def update
+    @article = article_find
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' updated!"
+
+    redirect_to articles_path
+  end
+
 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def article_find
+    Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
